de8906fb2994ae149562c81d932b3354da793b41
Gohar Simonyan <82412056+goharsimonyann@users.noreply.github.com>
2022-10-08 13:30:36 +0400

    updated binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,16 +1,19 @@
 function binarySearch(nums, target) {
-  let left = 0,
-    right = nums.length - 1,
-    mid;
+  let mid, left = 0,
+    right = nums.length - 1;
 
   while (right - left >= 1) {
     mid = Math.floor((right + left) / 2);
-
+    
+    if (nums[mid] === target) return mid;
+    
     if (nums[mid] < target) left = mid + 1;
     else right = mid;
   }
 
   if (nums[left] === target) return left;
-  else if (nums[right] === target) return right;
-  else return -1;
+  if (nums[right] === target) return right;
+  
+  return -1;
 }
+
