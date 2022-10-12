d16f0b6047736029c5afd28137082a9d04c7ccd4
Gohar Simonyan <82412056+goharsimonyann@users.noreply.github.com>
2022-10-12 22:04:29 +0400

    Updated Insertion_Sort.js

--- Insertion_Sort.js
+++ Insertion_Sort.js
@@ -12,20 +12,16 @@ function Insertion_Sort(arr) {
   return arr;
 }
 
-let arr = [2, 4, 842, 5];
-console.log(Insertion_Sort(arr));
-
-//  DESCENDING SORT
-
-// function Insertion_Sort(arr) {
-//     for (let i = 1; i < arr.length; ++i) {
-//       let j = i - 1;
-//       let key = arr[i];
-//       while (arr[j] > key && j >= 0) {
-//         arr[j + 1] = arr[j];
-//         --j;
-//       }
-//       arr[j + 1] = key;
-//     }
-//     return arr;
-//   }
+//  DESCENDING ORDER
+function Insertion_Sort(arr) {
+    for (let i = 1; i < arr.length; ++i) {
+      let j = i - 1;
+      let key = arr[i];
+      while (arr[j] > key && j >= 0) {
+        arr[j + 1] = arr[j];
+        --j;
+      }
+      arr[j + 1] = key;
+    }
+    return arr;
+  }
