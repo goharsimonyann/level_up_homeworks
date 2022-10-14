359bc18b0c513071c5dc79da24f4c0a5e905c1e0
Gohar Simonyan <82412056+goharsimonyann@users.noreply.github.com>
2022-10-14 21:16:21 +0400

    updated Insertion_Sort.js

--- Insertion_Sort.js
+++ Insertion_Sort.js
@@ -17,7 +17,7 @@ function Insertion_Sort(arr) {
     for (let i = 1; i < arr.length; ++i) {
       let j = i - 1;
       let key = arr[i];
-      while (arr[j] > key && j >= 0) {
+      while (arr[j] < key && j >= 0) {
         arr[j + 1] = arr[j];
         --j;
       }
